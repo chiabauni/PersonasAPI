d244c4544d2ddca1b8aac90e354e570d87b67556
Chiara Bauni <cbauni@fi.uba.ar>
2022-12-21 12:44:33 -0300

    Agrego controlador

--- PersonasAPI/Model/Persona.cs
+++ PersonasAPI/Model/Persona.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
 
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         public int Edad { get; set; }
 
-        public string Categoria { get; set; }
+        public string? Categoria { get; set; }
     }
 }
