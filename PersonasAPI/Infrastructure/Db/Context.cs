ad62757b5ee7422bad5a96f79221a1b7b6fc2476
Chiara Bauni <cbauni@fi.uba.ar>
2022-12-21 12:33:27 -0300

    Creacion de modelo y contexto + Migracion de db

---
+++ PersonasAPI/Infrastructure/Db/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonasAPI.Model;
+
+namespace PersonasAPI.Infrastructure.Db
+{
+    public class Context: DbContext
+    {
+        public DbSet<Persona> Persona { get; set; }
+        
+        public Context(DbContextOptions<Context> options)
+        : base(options)
+        {
+
+        }
+    }
+}
---
+++ PersonasAPI/Model/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonasAPI.Model
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Categoria { get; set; }
+    }
+}
